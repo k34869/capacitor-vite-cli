9c72fb8077821b3f62097535b484df1a67c7f6f3
k34869 <1838576587@qq.com>
2025-09-20 07:48:38 +0800

    :sparkles: cap-vite dev 95%

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-const fs = require("fs");
-const { execSync, spawn } = require("child_process");
 const { program } = require("commander");
 const { devModeStart } = require("./lib/dev");
 const package = require("./package.json");
@@ -16,7 +14,7 @@ program
   .command("create [template] [appName] [appId]")
   .description(`Create a project`)
   .action(() => {
-    console.log(666);
+    console.log("create develop...");
   });
 
 program
@@ -24,15 +22,19 @@ program
   .description(`Start vite dev server, then builds and deploys the native app`)
   .option("--host <host>", "Host used for live reload")
   .option("--port <port>", "Port used for live reload")
-  .action((platform) => {
-    devModeStart();
+  .action((platform, opts) => {
+    devModeStart({
+      platform,
+      host: opts.host,
+      port: opts.port,
+    });
   });
 
 program
   .command("build <platform>")
   .description("builds the release version of the selected platform")
   .action(() => {
-    console.log(777);
+    console.log("build develop...");
   });
 
 program.parse();
